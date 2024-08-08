d2d01772dfaa8b9793ec078624f64c84a792ce7f
nie.chao <nie.chao@advantech.com.cn>
2024-08-08 09:17:00 +0800

    shadowsocks

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -1,2 +1,1 @@
-docker run --name ssserver -e PASSWORD="158818" -e METHOD="aes-256-gcm" -e SERVER_PORT=8888 -p 8888:8888 -p 8888:8888/ud
-p -d --restart always shadowsocks/shadowsocks-libev
+docker run --name ssserver -e PASSWORD="158818" -e METHOD="aes-256-gcm" -e SERVER_PORT=8888 -p 8888:8888 -p 8888:8888/udp -d --restart always shadowsocks/shadowsocks-libev
